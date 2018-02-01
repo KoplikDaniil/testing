24c4201139fa9fe861d5e1e4bdfdedb859874bba
Даниил Коплик <daniil.koplik@yandex.ru>
2018-02-01 15:25:33 +0300

    Доработка путей до элементов.

--- csharp-example/TestingLogin.cs
+++ csharp-example/TestingLogin.cs
@@ -27,17 +27,28 @@ namespace csharp_example
             var rand = new Random();
 
             driver.Navigate().GoToUrl("https://www.yandex.ru/");
-            driver.FindElement(By.LinkText("Завести почту")).Click();
-            driver.FindElement(By.Name("firstname")).SendKeys("Test12345");
-            driver.FindElement(By.Name("lastname")).SendKeys("Test12345");
-            driver.FindElement(By.Name("login")).SendKeys("Test");
-            wait.Until(ExpectedConditions.TextToBePresentInElement(driver.FindElement(By.ClassName("form__login-suggest")), "К сожалению, логин занят"));
-            driver.FindElement(By.ClassName("login__suggest-button")).Click();
-            wait.Until(ExpectedConditions.TextToBePresentInElement(driver.FindElement(By.XPath("//*[@id='root']/div/div[2]/div/div[2]/div/div/div/form/div[1]/div[3]/div/div/div/div/div/strong")), "Свободные логины"));
-            IList <IWebElement> select = driver.FindElements(By.XPath( "//*[@id='root']/div/div[2]/div/div[2]/div/div/div/form/div[1]/div[3]/div/div/div/div/div/ul/li"));
-            //System.Threading.Thread.Sleep(10000);
-            select.ElementAtOrDefault(rand.Next(0, 10)).Click();
-            wait.Until(ExpectedConditions.TextToBePresentInElement(driver.FindElement(By.XPath("//*[@id='root']/div/div[2]/div/div[2]/div/div/div/form/div[1]/div[3]/div/div")), "Логин свободен"));
+            try
+            {
+                driver.FindElement(By.LinkText("Завести почту")).Click();
+                driver.FindElement(By.Name("firstname")).SendKeys("Test12345");
+                driver.FindElement(By.Name("lastname")).SendKeys("Test12345");
+                driver.FindElement(By.Name("login")).SendKeys("Test");
+                wait.Until(ExpectedConditions.TextToBePresentInElement(driver.FindElement(By.ClassName("form__login-suggest")), "К сожалению, логин занят"));
+                driver.FindElement(By.ClassName("login__suggest-button")).Click();
+                wait.Until(ExpectedConditions.TextToBePresentInElement(driver.FindElement(By.ClassName("suggest__logins")), "Свободные логины"));
+                IList<IWebElement> select = driver.FindElements(By.ClassName("registration__pseudo-link"));
+                select.ElementAtOrDefault(rand.Next(0, 10)).Click();
+                wait.Until(ExpectedConditions.TextToBePresentInElement(driver.FindElement(By.ClassName("reg-field__popup")), "Логин свободен"));
+               // System.Threading.Thread.Sleep(10000);
+            }
+            catch (NoSuchElementException ex)
+            {
+                Assert.Fail("[Selenium] Объект не найден!", ex);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("[Selenium] WTF exception: " + ex.ToString());
+            }
         }
 
         [TearDown]
